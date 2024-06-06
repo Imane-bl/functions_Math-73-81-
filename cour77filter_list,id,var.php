e52163cafaa6962cec9b65ba1d244f6c3b6f568d
imane <bellaghma.imane@gmail.com>
2024-06-06 14:46:47 +0100

    aded

---
+++ cour77filter_list,id,var.php
@@ -0,0 +1,32 @@
+<?php
+
+  /*
+    Filter Functions
+
+    - filter_list()
+    --- Returns A List Of All Supported Filters
+
+    - filter_id(Filter_Name[Required])
+
+    - filter_var(Value[Required], Filter[Optional], Options[Optional])
+    --- Filters A Variable With A Specified Filter
+  */
+
+  echo "<pre>";
+  print_r(filter_list());
+  echo "</pre>";
+
+  echo filter_id("boolean") . "<br>"; // 258
+  echo filter_id("int") . "<br>"; // 257
+
+  $val = "on"; // True || 1 || "yes" || "on"
+
+  if (filter_var($val, FILTER_VALIDATE_BOOL)) { // Or ID "258"
+
+    echo "This Is True";
+
+  } else {
+
+    echo "This Is False";
+
+  }
